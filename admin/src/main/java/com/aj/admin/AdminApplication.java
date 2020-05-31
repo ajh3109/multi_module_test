677a9aa7d981cf1969ea623e7a128426bb6c1771
ajh3109 <ajh3109@naver.com>
2020-05-31 15:14:14 +0900

    cicleci test

---
+++ admin/src/main/java/com/aj/admin/AdminApplication.java
@@ -0,0 +1,7 @@
+package com.aj.admin;
+
+public class AdminApplication {
+    public static void main(String[] args) {
+        System.out.println("Aadmin");
+    }
+}
---
+++ common/src/main/java/com/aj/common/CommonAppliction.java
@@ -0,0 +1,7 @@
+package com.aj.common;
+
+public class CommonAppliction {
+    public static void main(String[] args) {
+        System.out.println("Common");
+    }
+}
